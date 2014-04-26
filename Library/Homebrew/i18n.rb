f78b6f6e1ebd9a8e62dfb02664d8b3eabb8ad80a
Larry Gilbert <larry@L2G.to>
2014-04-26 14:44:14 -0700

    [i18nbrew] Add r18n-core and r18n-desktop gems to vendor directory

---
+++ Library/Homebrew/i18n.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH << File.expand_path('vendor/r18n-core/lib',    File.dirname(__FILE__))
+$LOAD_PATH << File.expand_path('vendor/r18n-desktop/lib', File.dirname(__FILE__))
+
+require 'r18n-desktop'
---
+++ Library/Homebrew/vendor/r18n-core/lib/r18n-core/version.rb
@@ -0,0 +1,4 @@
+# encoding: utf-8
+module R18n
+  VERSION = '1.1.10'.freeze unless defined? R18n::VERSION
+end
