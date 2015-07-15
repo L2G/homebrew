2bfefc997296348485b289c60abee6bd78ceab13
Larry Gilbert <larry@l2g.to>
2015-07-14 20:15:27 -0700

    dcraw: jpeg library change and cleanup
    * Bump rev for jpeg 9a
    * Other changes for `brew audit --strict`

--- Library/Formula/dcraw.rb
+++ Library/Formula/dcraw.rb
@@ -1,10 +1,9 @@
-require "formula"
-
 class Dcraw < Formula
   desc "Digital camera RAW photo decoding software"
   homepage "http://www.cybercom.net/~dcoffin/dcraw/"
   url "http://www.cybercom.net/~dcoffin/dcraw/archive/dcraw-9.22.tar.gz"
-  sha1 "f786f12d6cbf2a5571881bcf7f9945365459d433"
+  sha256 "0290aef2dc60fe434f8ecadf5f158c1c657c1d18b1adc6a91d33e5925d41bf01"
+  revision 1
 
   bottle do
     cellar :any
@@ -19,7 +18,8 @@ class Dcraw < Formula
 
   def install
     ENV.append_to_cflags "-I#{HOMEBREW_PREFIX}/include -L#{HOMEBREW_PREFIX}/lib"
-    system "#{ENV.cc} -o dcraw #{ENV.cflags} dcraw.c -lm -ljpeg -llcms2 -ljasper"
+    system ENV.cc, "-o", "dcraw", ENV.cflags, "dcraw.c", "-lm", "-ljpeg",
+       "-llcms2", "-ljasper"
     bin.install "dcraw"
     man1.install "dcraw.1"
   end
