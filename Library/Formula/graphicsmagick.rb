da55dfa8312366b8de738a9fe6ad055ffbaab2ce
Larry Gilbert <larry@l2g.to>
2015-07-17 16:15:51 -0700

    graphicsmagick: LARRY CHECK THIS AFTER SF IS UNBORKED

--- Library/Formula/graphicsmagick.rb
+++ Library/Formula/graphicsmagick.rb
@@ -5,6 +5,8 @@ class Graphicsmagick < Formula
   homepage 'http://www.graphicsmagick.org/'
   url 'https://downloads.sourceforge.net/project/graphicsmagick/graphicsmagick/1.3.21/GraphicsMagick-1.3.21.tar.bz2'
   sha256 'a0ce08f2710c158e39faa083463441f6eeeecce07dbd59510498ffa4e0b053d1'
+  revision 1
+
   head 'http://graphicsmagick.hg.sourceforge.net:8000/hgroot/graphicsmagick/graphicsmagick', :using => :hg
 
   bottle do
